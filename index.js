aee351f08cb9d7d0d9d6eb511431bcb997df3003
Jose Manuel Charro <jmcharrogarcia@gmail.com>
2020-08-01 14:08:31 +0200

    CORS y Express - primer commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// npm run start:dev
+
+// MONGO ATLAS
+// Usuario
+// jmainol
+// Password
+// JmRwyKT9oj4F8JtS
+
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Configurar CORS
+app.use( cors() );
+
+// Base de datos
+dbConnection();
+
+
+// Rutas
+app.get( '/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'Hola mundo'
+    })
+
+});
+
+app.listen( process.env.PORT, () => {
+    console.log( 'Servidor corriendo en puerto ' + process.env.PORT );
+});
